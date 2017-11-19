ef9b80240a1e6e6335967b9e7db83250dec9e7f4
Jørgen Juel <jorgen@notater.net>
2017-11-19 21:41:25 +0100

    Alle innlegg vises paa arrangement

--- lib/extras.php
+++ lib/extras.php
@@ -31,3 +31,21 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+
+
+/**
+ * Change query on some pages
+ */
+function custom_query_vars( $query ) {
+  if ( !is_admin() ){
+    if ( $query->query['post_type'] == 'arrangement' && $query->is_archive) {
+      // Include all arrangement cpts on archive page
+      if($query->query["posts_per_page"] != 3){
+        $query->set( 'posts_per_page', -1 );
+      }
+    }
+  }
+  return $query;
+}
+add_action( 'pre_get_posts', __NAMESPACE__ . '\\custom_query_vars' );
